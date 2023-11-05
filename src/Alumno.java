cceb86a236831a87736439f64d54762a929b9fe5
Jhonathan <jhonathana.vilgut@educa.jcyl>
2023-11-05 22:59:51 +0100

    creada estructura de clase: proyecto matricula_

---
+++ src/Alumno.java
@@ -0,0 +1,33 @@
+public class Alumno {
+
+
+    public String dni;
+    public String nombre;
+
+    public String apellidos;
+
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+}
---
+++ src/Matricula.java
@@ -0,0 +1,29 @@
+public class Matricula {
+    private String codio;
+    private String inicio;
+    private  String fin;
+
+    public String getCodio() {
+        return codio;
+    }
+
+    public void setCodio(String codio) {
+        this.codio = codio;
+    }
+
+    public String getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(String inicio) {
+        this.inicio = inicio;
+    }
+
+    public String getFin() {
+        return fin;
+    }
+
+    public void setFin(String fin) {
+        this.fin = fin;
+    }
+}
